3414b7630561d9d878412f3128aa648662d2ead8
Colin Jeanne <colinjeanne@hotmail.com>
2015-04-12 20:05:23 -0700

    Log each request

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,36 @@
+<?php namespace MealPlan\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\TerminableMiddleware;
+use Log;
+
+class LogRequestMiddleware implements TerminableMiddleware {
+   /**
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @return mixed
+    */
+   public function handle($request, Closure $next)
+   {
+      Log::info(
+         'Beginning request',
+         [
+            'time' => microtime(true),
+            'method' => $request->method(),
+            'uri' => $request->url()
+         ]);
+
+      return $next($request);
+   }
+
+   public function terminate($request, $response) {
+      Log::info(
+         'Ending request',
+         [
+            'time' => microtime(true),
+            'status' => $response->getStatusCode()
+         ]);
+   }
+}
